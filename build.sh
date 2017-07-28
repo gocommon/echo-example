2022f22cfbf9af04c4acdf8d518107766142fccc
weisd <weishidavip@163.com>
2017-07-28 16:17:03 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+if [ -f ./bin/main ];then
+mv ./bin/main ./bin/main.bak
+fi
 GITLOGVERSION=`git log --oneline --decorate --graph | sed -n 1p | awk '{print $2}'` 
 echo "start build $GITLOGVERSION"
 gb build -ldflags "-X main.GitLog=$GITLOGVERSION"
