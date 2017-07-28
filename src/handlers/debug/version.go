c32c389f23eb95d3c160562a696acc7d51d83543
weisd <weishidavip@163.com>
2017-07-28 15:16:52 +0800

    add responser

--- src/handlers/debug/version.go
+++ src/handlers/debug/version.go
@@ -4,11 +4,12 @@ import (
 	"net/http"
 	"setting"
 
+	"modules/responser"
+
 	"github.com/labstack/echo"
 )
 
 // Version Version
 func Version(c echo.Context) error {
-	c.Logger().Debugf("ver:%s", setting.Conf.Version)
-	return c.String(http.StatusOK, setting.Conf.Version)
+	return responser.R(c, http.StatusOK, setting.Conf.Version)
 }
